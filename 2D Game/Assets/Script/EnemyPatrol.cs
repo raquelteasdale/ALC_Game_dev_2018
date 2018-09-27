649d61647b79e1b1c909a5b72b8854becb3190eb
raquelteasdale <raquel.t265@stu.nebo.edu>
2018-09-27 10:31:27 -0600

    Created enemy

---
+++ 2D Game/Assets/Script/EnemyPatrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour {
+
+	// Movement Variables
+	public float MoveSpeed;
+	public bool MoveRight;
+	
+	// Wall Check
+	public float WallCheckRadius;
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+	
+	// Edge Check
+	private bool NotAtaEdge;
+	public Transform EdgeCheck;
+
+	
+	// Update is called once per frame
+	void Update () {
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+		
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+		
+		if (Hittingwall || !NotAtEdge){
+			MoveRight = !MoveRight;
+		}
+
+		if (MoveRight){
+			transform.localScale = new Vector3(-0.2f,0.2f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody>().velocity.y);
+		}
+		else {
+			transform.localScale = new Vector3(0.2f,0.2f,1f);
+			GetComponent<Rididbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody>().velocity.y);
+		}
+	}
+}
