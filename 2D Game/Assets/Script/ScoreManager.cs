3ab7eaa83638ce3c6d5e33672847deda121fe77a
raquelteasdale <raquel.t265@stu.nebo.edu>
2018-10-01 09:21:55 -0600

    Created score manager

---
+++ 2D Game/Assets/Script/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+public static int Score;
+Text Scoretext;
+	// Use this for initialization
+	void Start () {
+		ScoreTest = GetComponent<Text>();
+		Score = -1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		ScoreText.text = " " + Scene;
+		if (Score < 0)
+		{
+			Score = 0;
+		}
+	}
+
+	public static void AddPoints (int PointsToAdd)
+	{
+	Score += PointsToAdd;
+	}
+}
