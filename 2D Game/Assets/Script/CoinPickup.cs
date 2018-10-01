6bacdcc4765e87fcce430020c6ad57d1fe6bf89e
raquelteasdale <raquel.t265@stu.nebo.edu>
2018-10-01 09:21:03 -0600

    Created coin

---
+++ 2D Game/Assets/Script/CoinPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+	public int PointsToAdd;
+
+	void OnTriggerEnter2D (Collider2D other){
+
+		if (other.GetComponent<Rigidbody2D> () == null)
+			return;
+
+		ScoreManager.AddPoints (PointsToAdd);
+
+		Destroy (gameObject);
+	}
+}
