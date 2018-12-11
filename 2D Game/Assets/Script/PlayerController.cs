2ab382bf9a485eac6aec689c8d569520486cdaa7
raquelteasdale <raquel.t265@stu.nebo.edu>
2018-12-11 10:28:02 -0700

    animation

--- 2D Game/Assets/Script/PlayerController.cs
+++ 2D Game/Assets/Script/PlayerController.cs
@@ -17,11 +17,12 @@ public class PlayerController : MonoBehaviour {
 
 	//Non-Slide Player
 	private float moveVelocity;
-
+	public Animator animator;
 	
 	// Use this for initialization
 	void Start () {
-		
+		animator.SetBool("isWalking",false);
+		animator.SetBool("isJumping",false);
 	}
 
 	void FixedUpdate () {
@@ -37,8 +38,12 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		// Double jump code
-		if(grounded)
+		if(grounded){
 			doubleJump = false;
+			animator.SetBool("isJumping",false);
+		}
+
+
 
 		if(Input.GetKeyDown (KeyCode.Space)&& !doubleJump && !grounded){
 			Jump();
@@ -52,10 +57,21 @@ public class PlayerController : MonoBehaviour {
 		if(Input.GetKey (KeyCode.D)){
 			//GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 			moveVelocity = moveSpeed;
+			animator.SetBool("isWalking",true);
 		}
+
+		else if(Input.GetKeyUp (KeyCode.D)){
+			animator.SetBool("isWalking",false);
+		}
+
 		if(Input.GetKey (KeyCode.A)){
 			//GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 			moveVelocity = -moveSpeed;
+			animator.SetBool("isWalking",true);
+		}
+
+		else if(Input.GetKeyUp (KeyCode.A)){
+			animator.SetBool("isWalking",false);
 		}
 
 		GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
@@ -71,5 +87,6 @@ public class PlayerController : MonoBehaviour {
 
 	public void Jump(){
 		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		animator.SetBool("isJumping",true);
 	}
 }
